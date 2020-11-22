c1358015803ea14e6a0e2d0a9e006fad10c3061c
Leonardo H. Vincente <leonzkyhv@gmail.com>
2020-11-22 14:29:42 -0800

    Carrusel added working buttons

--- src/components/UI/Carrusel/Carrusel.js
+++ src/components/UI/Carrusel/Carrusel.js
@@ -4,6 +4,20 @@ import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 import classes from './Carrusel.module.css';
 
+const rightArrowClicked = (currentIndex, numberOfChildren, setCurrentIndex) => {
+    const newIndex = (currentIndex + 1) % numberOfChildren;
+    setCurrentIndex(newIndex);
+}
+
+
+const leftArrowClicked = (currentIndex, numberOfChildren, setCurrentIndex) => {
+    let newIndex = currentIndex - 1;
+    if (newIndex < 0) {
+        newIndex = numberOfChildren - 1;
+    }
+    setCurrentIndex(newIndex);
+}
+
 export default function Carrusel({ children }) {
     const [currentIndex, setCurrentIndex] = React.useState(0);
     const carruselItemToDisplay = children[currentIndex];
@@ -11,10 +25,14 @@ export default function Carrusel({ children }) {
     const rightArrowClasses = [classes.rightArrow, classes.arrowIcon].join(' ');
     return (
         <div className={classes.container} >
-            <div className={rightArrowClasses}>
+            <div className={rightArrowClasses} onClick={() => rightArrowClicked(currentIndex, children.length, setCurrentIndex)}>
                 <ArrowForwardIosIcon></ArrowForwardIosIcon>
             </div>
-            <ArrowBackIosIcon className={leftArrowClasses}></ArrowBackIosIcon>
+            <div className={leftArrowClasses}
+                onClick={() => leftArrowClicked(currentIndex, children.length, setCurrentIndex)}
+            >
+                <ArrowBackIosIcon></ArrowBackIosIcon>
+            </div>
             {carruselItemToDisplay}
         </div>
     );
