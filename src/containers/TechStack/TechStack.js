bae83b7a4dc9f17767f704b70d696c403815a9f0
Leonardo H. Vincente <leonzkyhv@gmail.com>
2021-01-04 20:39:15 -0800

    Added tech stack

--- src/containers/TechStack/TechStack.js
+++ src/containers/TechStack/TechStack.js
@@ -2,10 +2,74 @@ import React from 'react';
 
 import classes from './TechStack.module.css';
 
+const categories = [
+    {
+        name: 'Front End',
+        list: [
+            'React',
+            'Angular',
+            'Sass',
+            'Webpack',
+            'Jest',
+            'Jasmine',
+            'Electron',
+        ]
+    },
+    {
+        name: 'Back End',
+        list: [
+            'Java',
+            'JAX-RS',
+            'Node',
+            'Express',
+            'MySql',
+            'Jasmine',
+        ]
+    },
+    {
+        name: 'Mobile',
+        list: [
+            'Android',
+            'Java',
+            'Kotlin'
+        ]
+    },
+    {
+        name: 'Infrastructure',
+        list: [
+            'AWS',
+            'Jenkins',
+            'Docker',
+        ]
+    }
+];
+
 const TechStack = (props) => {
+    const contentItems = categories.map(item => {
+        const individualTech = item.list.map(tech => {
+            return (
+                <li>
+                    {tech}
+                </li>
+            );
+        })
+        return (
+            <div className={classes.category}>
+                <div className={classes.title}>{item.name}</div>
+                <div className={classes.description}>{individualTech}</div>
+            </div>
+        )
+    })
     return (
-        <div  id='techStack' className={classes.main}>
-            <div className={classes.techCardsContainer}> Tech Stack</div>
+        <div id='techStack' className={classes.main}>
+            <div className={classes.techCardsContainer}>
+                <div className={classes.header} >
+                    Tech Stack
+                </div>
+                <div className={classes.content}>
+                    {contentItems}
+                </div>
+            </div>
         </div>
     )
 }
