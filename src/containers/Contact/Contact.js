2f1489fdd7fcf4b27907002e1806e3c3c08899ee
Leonardo H. Vincente <leonzkyhv@gmail.com>
2021-01-10 19:00:20 -0800

    Added stle to contact

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -8,19 +8,21 @@ import classes from './Contact.module.css';
 
 const Contact = (props) => {
     return (
-        <div id='contact'>
+        <div id='contact' className={classes.contact}>
             <div className={classes.main}>
-                <div>
-                    <a href="https://www.linkedin.com/in/ingleonardohernandez/" target="_blank">
-                        <LinkedInIcon /></a>
-                </div>
-                <div>
-                    <a href="mailto: ic.lehevi@gmail.com"><EmailIcon /></a>
-                </div>
-                <div>
-                    <a href="https://github.com/LeonardoVincente" target="_blank">
-                        <GitHubIcon />
-                    </a>
+                <div className={classes.contactInfo} >
+                    <div>
+                        <a href="https://www.linkedin.com/in/ingleonardohernandez/" target="_blank">
+                            <LinkedInIcon /></a>
+                    </div>
+                    <div>
+                        <a href="mailto: ic.lehevi@gmail.com"><EmailIcon /></a>
+                    </div>
+                    <div>
+                        <a href="https://github.com/LeonardoVincente" target="_blank">
+                            <GitHubIcon />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
