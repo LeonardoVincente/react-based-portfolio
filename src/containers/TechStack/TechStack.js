0e520040297cbfff36e72cdf12aed6de58cdd630
Leonardo H. Vincente <leonzkyhv@gmail.com>
2021-03-24 21:47:27 -0700

    images changed location
    images changed location

--- src/containers/TechStack/TechStack.js
+++ src/containers/TechStack/TechStack.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import classes from './TechStack.module.css';
-import reactLogo from './react.svg';
+import reactLogo from './tech_stack/react.svg';
 
 
 const categories = [
