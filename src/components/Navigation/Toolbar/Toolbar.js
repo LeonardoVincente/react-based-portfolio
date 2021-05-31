3f448c27e5909eeafc5be782351677c88f24aee1
Leonardo H. Vincente <leonzkyhv@gmail.com>
2021-05-31 09:41:22 -0700

    Added icons

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,16 +3,16 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+import myLogo from './icon2-white.png';
 
 const toolbar = (props) => {
     return (
         <header className={classes.Toolbar}>
-            <DrawerToggle clicked={props.drawerToggleClicked}/>
-            <div className={classes.leo}>Leonardo Vincente</div>
-            {/* <div className={classes.Logo}>
-                <Logo height="80%" />
-            </div> */}
-
+            <DrawerToggle clicked={props.drawerToggleClicked} />
+            <div className={classes.leo}>
+                <img src={myLogo} width="24px" />
+                <span className={classes.textSpan}>Leonardo Vincente</span>
+            </div>
             <nav className={classes.DesktopOnly}>
                 <NavigationItems isAuthenticated={props.isAuth} />
             </nav>
